ed4581b71c5969c2f6f93785282be397461393c6
UdayaNarne <udayanarne08@gmail.com>
2025-04-30 16:07:20 +0530

    backend updates

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -2,7 +2,9 @@ const express=require('express');
 const router=express.Router();
 const {body,validationResult}=require('express-validator');
 const User=require('../models/Users');
-console.log(User.name);
+var bcrypt=require('bcryptjs');
+const jwt=require('jsonwebtoken');
+const JWT_SECRET="tokenthesecretkey";
 router.post('/createuser',[
     body('name').isLength({min:5,max:16}).withMessage('Name should be between 5 and 16 characters'),
     body('email').isEmail().withMessage('Email is not valid'),
@@ -17,11 +19,24 @@ router.post('/createuser',[
         if(user){
             return res.status(400).json({error:"User with this email already exists"});
         }
+        const salt=await bcrypt.genSalt(10);
+        const secPass=(await bcrypt.hash(req.body.password,salt));
+
         user=await User.create({
             name:req.body.name,
             email:req.body.email,
-            password:req.body.password
-        }).then(user=>res.json(user)).catch((err)=>res.status(500).json({error:err.message}));
+            password:secPass
+        });
+        const data={
+            user:{
+                id:user.id,
+            }
+        }
+        const jwtData=jwt.sign(data, JWT_SECRET);
+        console.log(jwtData);
+        console.log(user);
+        res.json()
+        //return 
     }
     catch(err){
         console.error(err.message);
