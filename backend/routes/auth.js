d0b6e53baa12bd1b4885c8f9a1d75c26148bfadc
UdayaNarne <udayanarne08@gmail.com>
2025-05-02 20:16:10 +0530

    Added more routes

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -7,6 +7,7 @@ const jwt=require('jsonwebtoken');
 const fetchuser=require('../middleware/fetchUser');
 const dotenv=require('dotenv').config({path:'../.env.local'});
 const JWT_SECRET=process.env.JWT_SECRET;
+const mongoose=require('mongoose');
 //Route 1: Creation of New User
 router.post('/createuser',[
     body('name').isLength({min:5,max:16}).withMessage('Name should be between 5 and 16 characters'),
@@ -49,19 +50,24 @@ router.post('/createuser',[
 })
 
 //Route 2: Login User
-router.post('/login',[
-    body('email').isEmail(),
-    body('password').isLength({min:8}).matches(/^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[\W_]).+$/).withMessage("Password should be atleast 8 charcaters long"),
-],async(req,res)=>{
+router.post('/login',async(req,res)=>{
     const {email,password}=req.body;
     let user1=await User.findOne({email:email});
     if(!user1){
-        res.status(400).json("Incorrect Email provided");
+        return res.status(400).json("Incorrect Email provided");
     }
-    let passwordCompare=await bcrypt.compare(password,user1.password);
+    const salt=await bcrypt.genSalt(10);
+    const secPass=(await bcrypt.hash(password,salt));
+
+    //let passwordCompare=await bcrypt.compare(user1.password,secPass);
+    let passwordCompare = await bcrypt.compare(password, user1.password);
+    console.log(password);
+    console.log(user1.password);
+    console.log(passwordCompare);
     if(!passwordCompare){
-        res.status(400).json("Incorrect Password provided");
+        return res.status(400).json("Incorrect Password provided");
     }
+
     const data={
         user:{
             id:user1.id,
@@ -82,4 +88,51 @@ router.post('/getUser',fetchuser,async(req,res)=>{
         res.status(500).send("Internal Server Error");
     }
 })
+
+//Route 4: Update User details
+router.put('/updateUser/:id',fetchuser,[
+    body('password').isLength({min:8}).matches(/^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[\W_]).+$/).withMessage("Password should be atleast 8 charcaters long"),
+],async(req,res)=>{
+    const {name,email,password}=req.body;
+    const errors=validationResult(req);
+    if(!errors.isEmpty()){
+        return res.status(400).json({errors:errors.array()});
+    }
+    let newUser={};
+    if(name){newUser.name=name};
+    if(email){newUser.email=email};
+    if(password){
+        const salt=await bcrypt.genSalt(10);
+        const secPass=(await bcrypt.hash(req.body.password,salt));
+        newUser.password=secPass;
+    }
+    let user1=await User.findById(req.params.id);
+    console.log("User1:",user1);
+    if(!user1){
+        return res.status(404).send("User not found");
+    }
+    if(user1.id.toString()!==req.user.id){
+        return res.status(401).send("Not allowed");
+    }
+    user1=await User.findByIdAndUpdate(req.params.id,{$set:newUser},{new:true});
+    res.json(user1);
+})
+//Route 5: Delete User
+router.delete('/deleteUser/:id',fetchuser,async(req,res)=>{
+    if (!mongoose.Types.ObjectId.isValid(req.params.id)) {
+        return res.status(400).send("Invalid user ID format");
+    }
+    console.log("Requested ID:", req.params.id);
+    let user1=await User.findById(req.params.id);
+    console.log("User1:",user1);
+    if(!user1){
+        return res.status(404).send("User not found");
+    }
+    if(user1.id.toString()!=req.user.id){
+        return res.status(401).send("Not allowed");
+    }
+    user1=await User.findByIdAndDelete(req.params.id);
+    res.json({success:true,message:"User has been deleted"});
+})
+
 module.exports=router;
--- backend/routes/notes.js
+++ backend/routes/notes.js
@@ -71,4 +71,24 @@ router.put('/updateNote/:id',fetchuser,[
     // }
 
 })
+
+//Route 4: Deleting a note
+router.delete('/deleteNote/:id',fetchuser,[
+    body('title').optional().isLength({min:3}).withMessage("Title should be atleast 3 characters"),
+    body('description').optional().isLength({min:5}).withMessage("Description shpuld be atleast 5 letters")
+],async(req,res)=>{
+    const errors=validationResult(req);
+    if(!errors.isEmpty()){
+        return res.send(400).json({errors:errors.array()});
+    }
+    let note=await Notes.findById(req.params.id);
+    if(!note){
+        return res.status(404).send("Note not found");
+    }
+    if(note.user.toString()!=req.user.id){
+        return res.status(401).send("Not allowed");
+    }
+    note=await Notes.findByIdAndDelete(req.params.id);
+    res.json({note});
+})
 module.exports=router;
