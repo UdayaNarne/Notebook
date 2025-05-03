839085dcc8844bfed5ffff82fe21cc335a276ccd
UdayaNarne <udayanarne08@gmail.com>
2025-05-03 17:23:04 +0530

    Updates

--- src/components/About.js
+++ src/components/About.js
@@ -1,14 +1,9 @@
-import { useEffect } from 'react';
-import { useContext } from 'react';
+import { useEffect,useContext } from 'react';
 import NoteContext from '../context/notes/NoteContext';
 export default function About(){
-    const a=useContext(NoteContext);
-    useEffect(()=>{
-       a.updateState();
-    });
     return(
         <> 
-            <h1>About Page {a.state2.name}</h1>
+            <h1>About Page</h1>
         </>
     )
 }
--- src/context/notes/NoteState.js
+++ src/context/notes/NoteState.js
@@ -2,28 +2,8 @@ import {useState} from 'react';
 import NoteContext from './NoteContext'
 
 const NoteState=(props)=>{
-    // const state={
-    //     "name":"Ravi",
-    //     "class":"5th",
-    //     "school":"St. Marys"
-    // };
-    const [state2,setState]=useState({
-        "name":"Ravi",
-        "class":"5th",
-        "school":"St. Marys"
-    });
-    const updateState=()=>{
-        setTimeout(()=>{
-            setState({
-                // ...state2,
-                "name":"Udaya",
-                "class":"10th",
-                "school":"St. Marys"
-            })
-        },1000);
-    }
     return(
-        <NoteContext.Provider value={{state2,updateState}}>
+        <NoteContext.Provider>
             {props.children}
         </NoteContext.Provider>
     )
